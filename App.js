7049ab0ff735164f6ab18343edcd990168e41750
EricJGardner <74524075+EricJGardner@users.noreply.github.com>
2020-12-23 14:20:53 -0600

    Backed up public and src folders
    will need to run npm and react in order for site to work , but these are my contributions to the project

---
+++ App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import Product1 from './Components/Product1';
+import Product2 from './Components/Product2';
+import Product3 from './Components/Product3';
+import Product4 from './Components/Product4';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+        </p>
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+        <Product1 />
+        <Product2 />
+        <Product3 />
+        <Product4 />
+      </header>
+    </div>
+  );
+}
+
+export default App;
